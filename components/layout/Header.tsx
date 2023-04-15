99fc77cdb1e16c90f069538bd1c3d9a2a0918965
kekelberi <alexei.gaponov@gmail.com>
2023-04-14 23:00:49 -0500

    save

--- components/layout/Header.tsx
+++ components/layout/Header.tsx
@@ -1,13 +1,30 @@
+import { ethers } from 'ethers';
 import Image from 'next/image';
+import { useEffect, useState } from 'react';
 
 const Header = () => {
+  const [account, setAccount] = useState<string | null>(null);
+
   const handleConnect = async () => {
-    if (!window.ethereum) {
-      console.log('da');
-    } 
-    console.log("window.ethereum",window.ethereum )
-    
+    try {
+      if (typeof window === 'undefined' || !window.ethereum) {
+        throw new Error('not connect...');
+      }
+
+      const accounts = await window.ethereum.request({
+        method: 'eth_requestAccounts',
+      });
+
+      const web3Provider = new ethers.JsonRpcProvider('http://localhost:3000');
+      console.log('web3Provider', web3Provider);
+
+      setAccount(accounts[0]);
+      console.log('account', accounts);
+    } catch (err) {
+      console.error(err);
+    }
   };
+
   return (
     <div className="header_wrap">
       <div className="header_main">
@@ -30,9 +47,11 @@ const Header = () => {
         <div className="header_menu_two">
           <div className="menu_two_dot">...</div>
           <div className="menu_two_sync">
-            <div className='zkSync_one'>zkSync</div>
+            <div className="zkSync_one">zkSync</div>
           </div>
-          <div onClick={handleConnect} className="menu_two_connect">Connect</div>
+          <div onClick={handleConnect} className="menu_two_connect">
+            {account ? 'yes' : 'no'}
+          </div>
         </div>
       </div>
     </div>
